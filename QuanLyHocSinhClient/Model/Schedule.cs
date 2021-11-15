1183ed3a6b63260313d6babec2a5c7a0aac5053b
tuanHoan <tuanHoan@DESKTOP-SK4QVPR>
2021-11-15 19:43:31 +0700

    change namespace

--- QuanLyHocSinhClient/Model/Schedule.cs
+++ QuanLyHocSinhClient/Model/Schedule.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QuanLyTKB.Models
+namespace QuanLyHocSinhClient.Models
 {
     public class Schedule
     {
--- QuanLyHocSinhClient/Model/Teacher.cs
+++ QuanLyHocSinhClient/Model/Teacher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QuanLyHocSinh.Models
+namespace QuanLyHocSinhClient.Models
 {
     public class Teacher
     {
